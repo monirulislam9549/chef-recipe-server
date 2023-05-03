93bf8cb6d6d098b064f4cf946bffe71e8547aef1
monirulislam9549 <monirulislam9549@gmail.com>
2023-05-03 14:44:13 +0600

    api is ready

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.get('/recipe', (req, res) => {
 
 app.get('/recipe/:id', (req, res) => {
     const id = req.params.id;
-    console.log(id);
+    // console.log(id);
     const selectedRecipeDetails = recipe.find(r => r.id == id)
     res.send(selectedRecipeDetails)
-    console.log(selectedRecipeDetails);
+    // console.log(selectedRecipeDetails);
 })
 
 
