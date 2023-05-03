a6d41c30a2801ad0f9e4e106d1ce38e514700b45
monirulislam9549 <monirulislam9549@gmail.com>
2023-05-03 14:41:03 +0600

    recipe json added

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const app = express()
 const port = process.env.PORT || 5000;
 app.use(cors())
 
+const recipe = require('./data/chefs.json')
+
 app.get('/', (req, res) => {
     res.send('recipe is running')
+})
+
+app.get('/recipe', (req, res) => {
+    res.send(recipe)
+})
+
+app.get('/recipe/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedRecipeDetails = recipe.find(r => r.id == id)
+    res.send(selectedRecipeDetails)
+    console.log(selectedRecipeDetails);
 })
 
 
